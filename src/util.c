88307fc89e48892dc363cb2c7200993db8b17387
ryuichi1208 <ryucrosskey@gmail.com>
2018-09-03 18:10:09 +0900

    util関数追加

--- src/util.c
+++ src/util.c
@@ -15,3 +15,33 @@ int _chdir(char *dir) {
 int _open(char *filepath) {
 	return 0;
 }
+
+size_t get_filesize(FILE *fp){
+	fseek(fp, 0L, SEEK_END);
+	size_t size = ftell(fp);
+	fseek(fp, 0L, SEEK_SET);
+	return size;
+}
+
+string_t* read_file(FILE *fp){
+	int c;
+	size_t i = 0;
+
+	size_t fsize = get_filesize(fp);
+
+	string_t *str = string_new(fsize);
+
+	while(c = fgetc(fp)){
+		if(c == EOF) break;
+		string_set(str, i, c);
+		i++;
+	}
+
+	return str;
+}
+
+void print_indent(size_t indent){
+	size_t i;
+	for(i=0;i<indent;i++) putchar(' ');
+}
+
