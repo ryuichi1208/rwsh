64a88cb4e25efb83a1e87e301f9144a195d8d5c1
Ryuichi Watanabe <ryucrosskey@gmail.com>
2019-10-17 22:32:51 +0900

    add test case

--- tests/test_rwsh.py
+++ tests/test_rwsh.py
@@ -4,6 +4,19 @@ import pytest
 sys.path.append('.')
 from rwsh import *
 
+class RoIAlign(Module):
+
+    def __init__(self, out_size, spatial_scale, sample_num=0):
+        super(RoIAlign, self).__init__()
+
+        self.out_size = out_size
+        self.spatial_scale = float(spatial_scale)
+        self.sample_num = int(sample_num)
+
+    def forward(self, features, rois):
+        return RoIAlignFunction.apply(features, rois, self.out_size,
+                                      self.spatial_scale, self.sample_num)
+
 def test_init_completion():
    assert init_completion() == None
 
